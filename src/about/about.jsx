d3bc8d1d113b9f43db1f2916f55bb610a9ee5c7e
Domenico <domedfd@gmail.com>
2019-09-04 16:29:04 -0400

    Creacion del componente PageHeader com el frameword bootstrap

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import PageHeader from "../template/pageHeader";
+
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name="About" small="Nos"></PageHeader>
+
+    <h2>Nossa Historia</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <h2>Misao e visao</h2>
+    <p>Lorem ipsum dolor sit amet..</p>
+    <h2>Impresa</h2>
+    <p>Lorem ipsum dolor sit amet..</p>
   </div>
 );
