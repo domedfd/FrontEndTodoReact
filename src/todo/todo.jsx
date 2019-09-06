5cf0252a4986a9968d72aca64c97ed5589a5eadf
Domenico <domedfd@gmail.com>
2019-09-06 09:01:16 -0400

    Creacion del construntor en todoform para poder usar el this con o methodo this

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -5,11 +5,20 @@ import TodoForm from "../todo/todoForm";
 import TodoList from "./todoList";
 
 export default class todo extends Component {
+  constructor(props) {
+    super(props);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    console.log(this);
+  }
+
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro" />
-        <TodoForm />
+        <TodoForm handleAdd={this.handleAdd} />
         <TodoList />
       </div>
     );
