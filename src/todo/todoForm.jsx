64a77e80640948477c5840caaad22f0e4c8b6510
Domenico <domedfd@gmail.com>
2019-09-06 14:24:28 -0400

    Arreglado error de digitacion de descripcion en value de todoform y tambien creacion de busqueda

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -10,12 +10,13 @@ export default props => (
         className="form-control"
         placeholder="Adcione uma tarefa"
         onChange={props.handleChange}
-        value={props.decription}
+        value={props.description}
       ></input>
     </Grid>
 
     <Grid cols="12 3 2">
       <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
+      <IconButton style="info" icon="search" onClick={props.handleSearch} />
     </Grid>
   </div>
 );
