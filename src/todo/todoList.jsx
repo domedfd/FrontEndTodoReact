19393d4bf15e8dacf35303aca02481e1c91364d9
Domenico <domedfd@gmail.com>
2019-09-13 11:40:34 -0400

    Marcar concluido e pendente

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import IconButton from "../template/iconButton";
+import { bindActionCreators } from "redux";
+import { markAsDone, markAsPending, remove } from "./todoAction";
 
 const TodoList = props => {
   const renderRows = () => {
@@ -13,19 +15,19 @@ const TodoList = props => {
             hide={todo.done}
             style="success"
             icon="check"
-            onClick={() => props.handleMarkAsDone(todo)}
+            onClick={() => props.markAsDone(todo)}
           />
           <IconButton
             hide={!todo.done}
             style="warning"
             icon="undo"
-            onClick={() => props.handleMarkAsPending(todo)}
+            onClick={() => props.markAsPending(todo)}
           />
           <IconButton
             hide={!todo.done}
             style="danger"
             icon="trash-o"
-            onClick={() => props.handleRemove(todo)}
+            onClick={() => props.remove(todo)}
           />
         </td>
       </tr>
@@ -70,4 +72,9 @@ const TodoList = props => {
 };
 
 const mapStateToProps = state => ({ list: state.todo.list });
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ markAsDone, markAsPending, remove }, dispatch);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
